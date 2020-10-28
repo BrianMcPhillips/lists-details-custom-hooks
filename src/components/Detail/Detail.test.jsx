fb9c5d2313ab36b27b0bc75eb34b487519ad65dd
Brian McPhillips <B.McPhillips89@gmail.com>
2020-10-27 23:55:37 -0700

    Detail and detail page setup

---
+++ src/components/Detail/Detail.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { getCharacterByName } from '../../services/xFilesApi';
+import Detail from './Detail';
+
+jest.mock('../../services/xFilesApi');
+
+describe('Detail component', () => {
+  it('display a character to detail page after load', () => {
+    getCharacterByName.mockResolvedValue({
+      name: 'Test Testerson',
+      status: 'Meh',
+      occupation: 'Crushing it full time',
+      image: 'http://www.placecage.com/200/200',
+      description: 'this is the best test ever written'
+    });
+
+    render();
+
+    screen.getByText('Loading!');
+
+    return waitFor(() => {
+      screen.getByText('Test Testerson');
+      screen.getByText('Meh');
+      screen.getByText('Crushing it full time');
+      screen.getByAltText('Test Testerson');
+      screen.getByText('this is the best test ever written');
+    });
+  });
+});
