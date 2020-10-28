9161f307b81a6de630fcc92ab511d8e49e911f2a
Brian McPhillips <B.McPhillips89@gmail.com>
2020-10-27 22:03:35 -0700

    List page working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import CharacterListPage from '../../pages/CharacterListPage';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <CharacterListPage />;
 }
---
+++ src/pages/CharacterListPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CharacterList from '../components/Characters/CharacterList';
+
+const CharacterListPage = () => {
+  return (
+    <>
+      <CharacterList />
+    </>
+  );
+};
+
+export default CharacterListPage;
